d32d1fcb1fac13d75b30fd64466f56bdf1ee1c27
Кощей бесстыжий Ayzek <87474130+MathematicLove@users.noreply.github.com>
2025-05-26 20:38:43 +0300

    Initial Commit

---
+++ MathPro/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MathPro
+//
+//  Created by Горе_Программист++ on 26.05.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
